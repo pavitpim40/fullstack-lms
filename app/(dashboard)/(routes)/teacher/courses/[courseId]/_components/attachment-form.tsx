8bfc8f9c9fa1ce381074b752e9fdc980327941f3
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-02 22:13:24 +0700

    attachment

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -0,0 +1,119 @@
+'use client';
+import { useState } from 'react';
+import * as z from 'zod';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
+import { Loader2, PlusCircle, X } from 'lucide-react';
+import toast from 'react-hot-toast';
+import { File } from 'lucide-react';
+import { Attachment, Course } from '@prisma/client';
+import { FileUpload } from '@/components/file-upload';
+
+interface AttachmentFormProps {
+  initialData: Course & { attachments: Attachment[] };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  url: z.string().min(1),
+});
+const AttachmentForm = ({ initialData, courseId }: AttachmentFormProps) => {
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+  const [deletingAttachmentId, setDeletingAttachmentId] = useState<string | null>(null);
+
+  const toggleEdit = () => setIsEditing((c) => !c);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    try {
+      await axios.post(`/api/courses/${courseId}/attachments`, values);
+      toast.success('Course updated');
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error('Something  went wrong');
+    }
+  };
+
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingAttachmentId(id);
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+      toast.success('Attachment deleted');
+      router.refresh();
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setDeletingAttachmentId(null);
+    }
+  };
+  return (
+    <div className='mt-6 border bg-slate-100 rounded-md p-4'>
+      <div className='font-medium flex items-center justify-between'>
+        Course attachments
+        <Button variant='ghost' onClick={toggleEdit}>
+          {isEditing && <>Cancel</>}
+          {!isEditing && (
+            <>
+              <PlusCircle className='h-4 w-4 mr-2' />
+              Add a file
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <>
+          {initialData.attachments.length === 0 && (
+            <p className='text-sm mt-2 text-slate-500 italic'>No attachments yet</p>
+          )}
+        </>
+      )}
+      {initialData.attachments.length > 0 && (
+        <div className='space-y-2'>
+          {initialData.attachments.map((attachment) => (
+            <div
+              key={attachment.id}
+              className='flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md'
+            >
+              <File className='h-4 w-4 mr-2 flex-shrink-0' />
+              <p className='text-xs line-clamp-1'>{attachment.name}</p>
+              {deletingAttachmentId === attachment.id && (
+                <div>
+                  <Loader2 className='h-4  w-4 animate-spin' />
+                </div>
+              )}
+              {deletingAttachmentId !== attachment.id && (
+                <button
+                  className='ml-auto hover:opacity-75 transition'
+                  onClick={() => onDelete(attachment.id)}
+                >
+                  <X className='h-4  w-4 ' />
+                </button>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint='courseAttachment'
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ url: url });
+              }
+            }}
+          />
+          <div className='text-xs text-muted-foreground mt-4'>
+            Add anything your student might need to complete the course.
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AttachmentForm;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,14 +5,12 @@ const DashBoardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-full'>
       <div className='h-[80px] md:pl-56 fixed inset-y-0 w-full z-50'>
-          <Navbar />
+        <Navbar />
       </div>
       <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0'>
         <SideBar />
       </div>
-      <main className='md:pl-56 pt-[80px] h-full'>
-      {children}
-      </main>
+      <main className='md:pl-56 pt-[80px] h-full'>{children}</main>
     </div>
   );
 };
